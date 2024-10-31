0f24770423e18c989979543bed89308a34756cee
Seung-Woo Seo <seungwoo103@gmail.com>
2024-10-31 15:04:48 -0400

    created folders and files, created code lines

---
+++ src/layout/Root.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+
+function Root() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
+
+export default Root;
