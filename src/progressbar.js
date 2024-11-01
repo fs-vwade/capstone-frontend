5bc52bcf8cd6d1632146886b1be539ac760e6a1e
Seung-Woo Seo <seungwoo103@gmail.com>
2024-10-31 21:34:22 -0400

    decided to use usestate

--- src/progressbar.js
+++ src/progressbar.js
@@ -1,3 +1,1 @@
-//may need to add module here if we are to use ProgressBar but I'm writing code that does not require it, if we want to change it later, please let me know
-
-const progressBar = document.getElementById("p")
+import React, { useState } from 'react';
