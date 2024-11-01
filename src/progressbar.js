68c7d2094cbddc9cfde665913b9296d1c0c65f91
Seung-Woo Seo <seungwoo103@gmail.com>
2024-10-31 21:44:08 -0400

    added main portion of the progressbar codes, except the dimensons

--- src/progressbar.js
+++ src/progressbar.js
@@ -1,1 +1,19 @@
 import React, { useState } from 'react';
+
+const ProgressBar = ({ completed }) => {
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setProgress((prevProgress) => {
+                if (prevProgress >= completed) {
+                    clearInterval(interval);
+                    return completed;
+                }
+                return prevProgress + 1;
+            });
+        }, 10);
+
+        return () => clearInterval(interval);
+    }, [completed]);
+}
