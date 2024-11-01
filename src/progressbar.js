79527df9f94b7e04b5ae0c1dcdcff715dbe05b1e
Seung-Woo Seo <seungwoo103@gmail.com>
2024-10-31 21:13:19 -0400

    added rest of the width spacing the bar would take up to show its in progress

--- src/progressbar.js
+++ src/progressbar.js
@@ -7,6 +7,14 @@ function move() {
         const elem = document.getElementById("progessBar");
         const width = 1;
         const id = setInterval(frame, 10);
-        
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
     }
 }
