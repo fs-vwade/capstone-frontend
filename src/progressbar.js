8adadbfa1e8ba917be1324b78daa4c3510bd581e
Seung-Woo Seo <seungwoo103@gmail.com>
2024-10-31 21:11:19 -0400

    added dimensons for the progress bar so far

--- src/progressbar.js
+++ src/progressbar.js
@@ -1,3 +1,12 @@
 //may need to add module here if we are to use ProgressBar but I'm writing code that does not require it, if we want to change it later, please let me know
 
 const i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        const elem = document.getElementById("progessBar");
+        const width = 1;
+        const id = setInterval(frame, 10);
+        
+    }
+}
