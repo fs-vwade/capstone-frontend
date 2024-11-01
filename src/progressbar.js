cf15e4b1e5304f1e3c03b2fe717fe18831cb7069
Seung-Woo Seo <seungwoo103@gmail.com>
2024-10-31 21:24:49 -0400

    started with progressbar code line

--- src/progressbar.js
+++ src/progressbar.js
@@ -1,1 +1,3 @@
 //may need to add module here if we are to use ProgressBar but I'm writing code that does not require it, if we want to change it later, please let me know
+
+const progressBar = document.getElementById("p")
