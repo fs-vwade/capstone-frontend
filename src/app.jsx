7af0c3272ed52f9fe1f9652802ae76fc568c0d1e
Seung-Woo Seo <seungwoo103@gmail.com>
2024-10-31 22:24:21 -0400

    added comment on app.jsx regarding ProgressBar import

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import ProgressBar from "./ProgressBar";
+// import ProgressBar from "./ProgressBar"; - not sure if this module? or just folder path
 
 const App = () => {
   return (
