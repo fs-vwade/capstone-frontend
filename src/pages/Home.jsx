c2ca238d5045af21edaf2dca162840ec858b6d04
Victor Wade <p9859817888@gmail.com>
2024-11-05 10:47:24 -1000

    commented/removed unused imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 // src/pages/Home.jsx
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setCredentials } from "../../api/authSlice";
