3925f66261292bdca7dbb7f2a9d74ab468b36b63
Victor Wade <p9859817888@gmail.com>
2024-11-06 10:16:40 -1000

    api url `PORT` should be set to `3000`
    - as it is in `backend/express/server.js`

--- api/store/api.js
+++ api/store/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_URL = `http://localhost:1337/`;
+const API_URL = `http://localhost:3000/`;
 
 const api = createApi({
 	baseQuery: fetchBaseQuery({
