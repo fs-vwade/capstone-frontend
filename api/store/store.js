13eefab2fa02f00121d2968bbf33c7b0f49ea6dd
Victor Wade <p9859817888@gmail.com>
2024-11-06 09:10:20 -1000

    cross-referenced `BookBuddy`

--- api/store/store.js
+++ api/store/store.js
@@ -1,15 +1,16 @@
 // src/features/store.js
 import { configureStore } from "@reduxjs/toolkit";
+
+import api from "./api";
 import authReducer from "../authSlice";
-import { projectsApi } from "../projectSlice";
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
-		[projectsApi.reducerPath]: projectsApi.reducer, // Add the API reducer
+		[api.reducerPath]: api.reducer, // Add the API reducer
 	},
 	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(projectsApi.middleware), // Add the API middleware
+		getDefaultMiddleware().concat(api.middleware), // Add the API middleware
 });
 
 export default store;
