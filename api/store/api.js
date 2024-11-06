49841f9fa42bddc60c7b2c2cd6cf3a521d6032b6
Victor Wade <p9859817888@gmail.com>
2024-11-06 09:09:33 -1000

    syntax

--- api/store/api.js
+++ api/store/api.js
@@ -5,13 +5,13 @@ const API_URL = `http://localhost:1337/`;
 const api = createApi({
 	baseQuery: fetchBaseQuery({
 		baseUrl: encodeURI(API_URL),
+		prepareHeaders: (headers, { getState }) => {
+			const token = getState().auth.token;
+			headers.set("content-type", `application/json`);
+			token && headers.set("authorization", `Bearer ${token}`);
+			return headers;
+		},
 	}),
-	prepareHeaders: (headers, { getState }) => {
-		const token = getState().auth.token;
-		headers.set("content-type", `application/json`);
-		if (token) headers.set("authorization", `Bearer ${token}`);
-		return headers;
-	},
 	endpoints: () => ({}),
 	tagTypes: ["Auth", "Student", "Project"],
 });
