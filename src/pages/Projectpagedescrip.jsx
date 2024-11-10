e8b4fa6131e79fbca3ad0ee205ffc337a27dd03a
Seung-Woo Seo <seungwoo103@gmail.com>
2024-11-10 18:23:50 -0500

    added textarea rows and columns

--- src/pages/Projectpagedescrip.jsx
+++ src/pages/Projectpagedescrip.jsx
@@ -1,6 +1,6 @@
-
-
-const textbox = document.createElement("input");
-textbox.type = "text";
-textbox.id = "project.description";
-text
+const textarea = document.createElement("input");
+textarea.rows = 5;
+textarea.cols = 10;
+textarea.type = "text";
+textarea.placeholder = "Project Description"; //I don't know what you would like to put here
+textarea.id = "project.description";
